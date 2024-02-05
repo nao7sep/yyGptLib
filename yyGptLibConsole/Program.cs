c6f26562a252e29400600ca9002902ec2b1116c3
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-02-05 12:56:29 +0900

    Minor fix

--- yyGptLibConsole/Program.cs
+++ yyGptLibConsole/Program.cs
@@ -12,10 +12,14 @@ namespace yyGptLibConsole
             {
                 // Tester1.Test ();
                 // Tester2.Test ();
+
                 // Tester3.Test (100); // Do not run this test casually as it might cost a lot of money.
 
                 Tester3.GeneratePage (@"C:\Repositories\Resources\Static\Beautiful People and Places",
-                    "100 images generated in February 2024 with https://github.com/nao7sep/yyGpt/blob/main/yyGptLibConsole/Tester3.cs for testing purposes.");
+                    "100 images generated with the following code for testing purposes in February 2024:" +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    "https://github.com/nao7sep/yyGpt/blob/main/yyGptLibConsole/Tester3.cs");
             }
 
             catch (Exception xException)
